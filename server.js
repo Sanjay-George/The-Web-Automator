66df4aaa9e238aa6e24f70ada1ff4ffe2f8377d9
cw-sanjaygeorge <sanjay.george@carwale.com>
2021-10-17 10:02:04 +0530

    Minor changes in server.js
    Signed-off-by: cw-sanjaygeorge <sanjay.george@carwale.com>

--- server.js
+++ server.js
@@ -3,42 +3,44 @@ const pageHelper = require('./modules/common/pageHelper');
 let configChain = [];
 const automator = require('./modules/automation/automator');
 
-const url = "https://www.bikewale.com";
+const url = "https://www.carwale.com/";
 
 (async () => {
-    // const browser = await puppeteer.launch({ headless: false, defaultViewport: null} );
-    // let page = await pageHelper.openTab(browser, url);
+	const browser = await puppeteer.launch({ headless: false, defaultViewport: null} );
+    let page = await pageHelper.openTab(browser, url);
 
-	// await insertStyles(page);
-	// await insertScripts(page);
-	// await exposeFunctions(page);
+	await insertStyles(page);
+	await insertScripts(page);
+	await exposeFunctions(page);
 
-	// page.on('domcontentloaded', async () => {
-	// 	// insert all styles and scripts
-	// 	console.log(`DOM loaded: ${page.url()}`);
-	// 	await insertStyles(page);
-	// 	await insertScripts(page);
-	// 	await exposeFunctions(page);
+	page.on('domcontentloaded', async () => {
+		// insert all styles and scripts
+		console.log(`DOM loaded: ${page.url()}`);
+		await insertStyles(page);
+		await insertScripts(page);
+		await exposeFunctions(page);
 
-	// });
+	});
 
-	// page.on('dialog', async dialog => {
-	// 	console.log(dialog.message());
-	// 	await dialog.dismiss();
-	// });
+	page.on('dialog', async dialog => {
+		console.log(dialog.message());
+		await dialog.dismiss();
+	});
 
-	// page.on('close', async () => {
-	// 	// get all data from browser (ConfigManager module) and store in file
-	// 	console.log("page closed");
+	page.on('close', async () => {
+		// get all data from browser (ConfigManager module) and store in file
+		console.log("page closed");
 
-	// 	if(!configChain.length) 	return;
+		if(!configChain.length) 	return;
 
-	// 	await page.waitForTimeout(1000);
-	// 	await automator.initiate(url, configChain);
-	// });
+		await page.waitForTimeout(2000);
+		await automator.initiate(url, configChain);
+	});
 
-	configChain = [{"configType":1,"actionName":"select make","actionType":"1","actionKey":"makes","selectedLabels":["div#brand-type-container > ul:nth-child(1) > li:nth-child(1) > a > span.brand-type-title"],"selectedTargets":["div#brand-type-container > ul:nth-child(1) > li:nth-child(1) > a > span.brand-type-title"],"selectSimilar":true,"selectSiblings":false},{"configType":1,"actionName":"select 2nd models","actionType":"1","actionKey":"model","selectedLabels":["ul#listitems > li:nth-child(2) > div > div.bikeDescWrapper > h3 > a"],"selectedTargets":["ul#listitems > li:nth-child(2) > div > div.bikeDescWrapper > h3 > a"],"selectSimilar":false,"selectSiblings":false},{"configType":2,"stateName":"collect version price","stateType":"1","stateKey":"versionPrice","selectedLabels":["div#version-prices-grid > table > tbody > tr:nth-child(1) > td.font14 > p"],"selectedTargets":["div#version-prices-grid > table > tbody > tr:nth-child(1) > td.font13 > span.text-bold"],"selectSimilar":true,"selectSiblings":false,"performAfter":"1"}];
-	await automator.initiate(url, configChain);
+// 	configChain = [{"configType":1,"actionName":"click just launched","actionType":"1","actionKey":"","selectedLabels":[],"selectedTargets":["div#root > div:nth-child(4) > div:nth-child(2) > section > div > div > div:nth-child(1) > div > ul > li:nth-child(2) > div > span"],"selectSimilar":false,"selectSiblings":false},{"configType":1,"actionName":"view all just launched","actionType":"1","actionKey":"","selectedLabels":[],"selectedTargets":["div#root > div:nth-child(4) > div:nth-child(2) > section > div > div > div.o-brXWGL > div.o-dJmcbh > div > div > div.o-fznJzb.o-cVfYDK.o-fznJEv.o-chNNuk.o-fznJFh.o-cPFhqO > a > div"],"selectSimilar":false,"selectSiblings":false},{"configType":2,"stateName":"Scrape Model prices","stateType":"1","stateKey":"modelPrices","selectedLabels":["div#root > div.o-bWHzMb.o-ducbvd.o-cglRxs._1paG1N.o-fpkJwH.o-dCyDMp > div._39XvcL._1VfD2h > div.o-dpDliG.o-eAyrtt.o-cglRxs._3xYDcP.o-fpkJwH.o-dCyDMp > section.o-fzoHBq.o-fzptYr.o-brXWGL.o-fznJzb > div > ul > li:nth-child(2) > div > div > div > a > h3"],"selectedTargets":["div#root > div.o-bWHzMb.o-ducbvd.o-cglRxs._1paG1N.o-fpkJwH.o-dCyDMp > div._39XvcL._1VfD2h > div.o-dpDliG.o-eAyrtt.o-cglRxs._3xYDcP.o-fpkJwH.o-dCyDMp >section.o-fzoHBq.o-fzptYr.o-brXWGL.o-fznJzb > div > ul > li:nth-child(3) > div > div > div > div > span.o-Hyyko.o-cyHybq.o-eZTujG.o-eqqVmt"],"selectSimilar":true,"selectSiblings":false,"performAfter":"1"}]
+// ;
+	
+// 	await automator.initiate(url, configChain);
 		
 })();
 
@@ -46,18 +48,26 @@ const url = "https://www.bikewale.com";
 // but sometimes functions aren't exposing on first try. 
 // temporary fix - expose functions on page reload and catch the error 'method already exists'
 const exposeFunctions = async (page) => {
+	console.log('\nexposeFunctions - exposing getConfigChain');
 	try {
 		await page.exposeFunction('getConfigChain', () => configChain);
 	}
-	catch(ex) {}
+	catch(ex) {
+		console.error("exposeFunctions - failed exposing getConfigChain");
+		// console.trace();
+	}
 
+	console.log('exposeFunctions - exposing setConfigChain');
 	try {
 		await page.exposeFunction('setConfigChain', chain => {
 			console.log(JSON.stringify(chain));
 			configChain = chain ;
 		});
 	}
-	catch(ex) {}
+	catch(ex) {
+		console.error("exposeFunctions - failed exposing setConfigChain.");
+		// console.trace();
+	}
 };
 
 const insertStyles = async (page) => {
